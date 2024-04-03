5fe119cdeddb297361e64ab010b6b7e9f8ef86e2
mana9512 <manashreepatel28@gmail.com>
2024-04-03 16:41:43 -0400

    updated url

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ functions.cloudEvent('sendVerificationEmail', async cloudEvent => {
     const verificationToken = Buffer.from(JSON.stringify(data)).toString('base64');
 
     // Construct the verification link (replace with your actual domain)
-    const verificationLink = `https://manashree.me:443/v1/user/verify-email?token=${verificationToken}`;
+    const verificationLink = `https://manashree.me/v1/user/verify-email?token=${verificationToken}`;
 
 
     // Email options
